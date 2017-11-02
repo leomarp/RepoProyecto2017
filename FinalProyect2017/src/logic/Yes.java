16090eb06f26a54e033d9875c359cf10f3d91250
chess <chess@DESKTOP-45QK9EU>
2017-11-02 15:01:49 +0000

    probando en clases

--- FinalProyect2017/src/logic/Yes.java
+++ FinalProyect2017/src/logic/Yes.java
@@ -1,5 +1,17 @@
 package logic;
 
 public class Yes {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
 
 }
